6271d505f0d6fd272a7079605ff45eeced129320
Kaifengggg <112594768+Kaifengggg@users.noreply.github.com>
2022-12-21 16:38:29 +1100

    Create AI.py

---
+++ AI.py
@@ -0,0 +1,11 @@
+from constant import offsets
+class AI:
+    def __init__(self):
+        self.AI_3_continue_chessman_ = 0
+        self.AI_3_continue_chessman = 0
+        self.AI_4_continue_chessman_ = 0
+        self.AI_4_continue_chessman = 0
+        self.people_3_continue_chessman_ = 0
+        self.people_3_continue_chessman = 0
+        self.people_4_continue_chessman_ = 0
+        self.people_4_continue_chessman = 0
